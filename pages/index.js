756b7f6ec6d34e951f1ac6922c442c2840924e12
angel <angelsb94@gmail.com>
2020-04-01 19:05:30 -0600

    cambios de el home

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-import { TextStyle, Page, Layout, EmptyState } from '@shopify/polaris'
+import { 
+  TextStyle,
+  Page,
+  Layout,
+  EmptyState,
+  Card
+} from '@shopify/polaris'
 
 import {TitleBar, ResourcePicker } from '@shopify/app-bridge-react'
 
@@ -19,7 +25,7 @@ const Index = () => {
   <Page>
     <TitleBar
       primaryAction={{
-        content: 'Hola soy un primary action'
+        content: 'Hola cambie'
       }}
     />
 
@@ -31,15 +37,38 @@ const Index = () => {
       onCancel={() => setOpen(false)}
     />
     <Layout>
-      <EmptyState heading="Soy un titulo"
-        action={{
-          content: 'da click',
-          onAction: ()=> setOpen(true)
-        }}
-        image={img}
-      >
-        <p>Seleciona productos</p>
-      </EmptyState>
+      <Layout.Section>
+        <Card title="Bienvenido al Home" sectioned>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+           nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum.
+        </p>
+        </Card>
+      </Layout.Section>
+      <Layout.Section secondary>
+          <EmptyState
+            action={{
+            content: 'Registratre',
+            onAction: ()=> console.log('entre a registro')
+            }}
+          >
+          </EmptyState>
+          <EmptyState
+            action={{
+            content: 'Enviar productos a revision',
+            onAction: ()=> setOpen(true)
+            }}
+          >
+          </EmptyState>
+          <Card sectioned>
+            <p>La revision de los productos<br></br>
+            puede tomar de 24 a 48 horas</p>
+          </Card>
+      </Layout.Section>
     </Layout>
   </Page>
   )
